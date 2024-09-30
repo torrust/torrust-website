1845f2fe002c678702d4ccf23028d768a7c2a0a6
Jose Celano <josecelano@gmail.com>
2024-09-30 17:35:08 +0100

    feat: improve contents for self-host page

--- src/routes/(pages)/self-host/+page.svelte
+++ src/routes/(pages)/self-host/+page.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
 	import TableOfContents from '$lib/components/atoms/TableOfContents.svelte'; // Adjust the path if needed
+	import Callout from '$lib/components/molecules/Callout.svelte';
 
 	// Array of section objects with display names and IDs
 	let sections = [
-		{ name: 'Torrent solution (Index + Tracker)', id: 'torrustSolution' },
-		{ name: 'Build from sources (Rust)', id: 'buildSources' },
-		{ name: 'Docker', id: 'docker' },
-		{ name: 'Tutorials', id: 'tutorials' },
-		{ name: 'Torrust tracker', id: 'torrustTracker' }
+		{ name: 'Torrent Solution (Index + Tracker)', id: 'torrustSolution' },
+		{ name: 'Torrust Index', id: 'torrustIndex' },
+		{ name: 'Torrust Tracker', id: 'torrustTracker' }
 	];
 
 	let activeSection = '';
@@ -15,39 +14,105 @@
 
 <div>
 	<h1>Self-host</h1>
+
 	<div class="layout">
 		<div>
 			<TableOfContents {sections} {activeSection} />
 		</div>
+
 		<div class="content">
-			<h2 id="torrustSolution">Torrent solution (Index + Tracker)</h2>
+			<h2 id="torrustSolution">Torrust Solution (Index + Tracker)</h2>
 
 			<p>
-				The Torrust Solution is a complete installation of bot a BitTorrent Index which is
-				associated to one Torrust Tracker. Downloaded torrents will include the the associated
-				tracker in the list of trackers.
+				Considering hosting your own torrent site? Torrust offers a comprehensive solution with both
+				the Torrust <a href="torrust-index">Index</a>
+				and the <a href="torrust-tracker">Tracker</a>.
 			</p>
+
 			<p>
-				In our tutorial we explain how to install both in the same server using a single docker
-				compose configiration, but if you need more resources you could install the tracker and the
-				index in different servers. You just need to spits the docker compose configuration and
-				change the tracker configiration in the index to connect to the external tracker.
+				The Torrust Tracker is integrated with the Index, ensuring that downloaded torrents
+				automatically include it in their tracker list.
 			</p>
 
-			<h2 id="buildSources">Build from sources (Rust)</h2>
+			<p>
+				Torrust is highly flexible. You can run both the Tracker and Index together on the same
+				server, or set them up separately. You can choose to build from source or use pre-built
+				containers. If you don't expect heavy traffic, we recommend using Docker Compose to simplify
+				setup. For detailed instructions, refer to our guide on <a
+					href="deploying-torrust-to-production">Deploying Torrust to Production</a
+				>.
+			</p>
 
-			<p>TODO</p>
-			<h2 id="docker">Docker</h2>
+			<Callout type="warning">
+				Note: You can install the Tracker independently, but the Index cannot function without the
+				Torrust Tracker.
+			</Callout>
 
-			<p>TODO</p>
+			<p>
+				Our tutorial demonstrates how to install both the Tracker and Index on a single server using
+				a shared Docker Compose configuration. However, if your deployment requires more resources,
+				you can host the Tracker and Index on separate servers. In that case, modify the Docker
+				Compose setup to split the services, and configure the Index to connect to the external
+				Tracker.
+			</p>
 
-			<h2 id="tutorials">Tutorials</h2>
+			<p>For more information on installation, refer to the following resources:</p>
 
-			<p>TODO</p>
+			<ul>
+				<li>
+					<a href="https://docs.rs/torrust-tracker/latest/torrust_tracker/">Tracker Documentation</a
+					>
+				</li>
+				<li>
+					<a href="https://docs.rs/torrust-tracker/latest/torrust_index/">Index Documentation</a>
+				</li>
+				<li>
+					<a href="https://github.com/torrust/torrust-tracker/blob/develop/docs/containers.md"
+						>Tracker Containerization Guide</a
+					>
+				</li>
+				<li>
+					<a href="https://github.com/torrust/torrust-index/blob/develop/docs/containers.md"
+						>Index Containerization Guide</a
+					>
+				</li>
+				<li>
+					<a href="https://github.com/torrust/torrust-compose">Torrust Compose Repository</a> (Docker
+					Compose setups)
+				</li>
+				<li>
+					<a href="https://github.com/torrust/torrust-demo">Torrust Demo Repository</a> (issues and feedback)
+				</li>
+				<li><a href="http://localhost:5173/tags/Deployment">Deployment Tutorials</a> (blog)</li>
+			</ul>
 
-			<h2 id="torrustTracker">Torrust tracker</h2>
+			<Callout type="info">
+				If you're not familiar with system administration, we recommend using Docker Compose for
+				installation. It simplifies the process and provides automated certificate provisioning and
+				renewal.
+			</Callout>
 
-			<p>TODO</p>
+			<h2 id="torrustIndex">Torrust Index</h2>
+
+			<p>
+				For instructions on installing the Index independently, visit the <a
+					href="torrent-index#installation">installation section</a
+				> where you'll find guides for both source-based and Docker installations.
+			</p>
+
+			<h2 id="torrustTracker">Torrust Tracker</h2>
+
+			<p>
+				If you choose to install only the Tracker, please refer to the <a
+					href="torrent-tracker#installation">installation section</a
+				> for detailed instructions on building from source or using Docker.
+			</p>
+
+			<p>
+				If your tracker traffic is high, you might prefer to run the Tracker on a separate server.
+				To do so, simply remove the Tracker service from your Docker Compose configuration (if
+				you're using Docker Compose) and set it up independently using source builds or containers.
+			</p>
 		</div>
 	</div>
 </div>
